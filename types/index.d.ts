4084fbc68cb7cd7c3f046db384b62d39293c641c
shannon code <genecyber@gmail.com>
2024-01-17 19:40:15 -0500

    add types/index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,22 @@
+import { CuratedCollectionsResponse, MetaData, Vault } from './types';
+declare class EmblemVaultSDK {
+    private apiKey;
+    private baseUrl;
+    constructor(apiKey: string, baseUrl?: string);
+    generateUploadUrl(): void;
+    getAssetMetadata(projectName: string, strict?: boolean): any[];
+    getAllProjects(): any[];
+    fetchCuratedContracts(hideUnMintable?: boolean, overrideFunc?: Function | boolean): Promise<CuratedCollectionsResponse>;
+    createCuratedVault(template: any): Promise<Vault>;
+    fetchMetadata(tokenId: string): Promise<MetaData>;
+    fetchVaultsOfType(vaultType: string, address: string): Promise<any>;
+    loadWeb3(): Promise<import("web3").default<import("web3-eth").RegisteredSubscription> | undefined>;
+}
+declare global {
+    interface Window {
+        EmblemVaultSDK: any;
+        web3: any;
+        ethereum: any;
+    }
+}
+export default EmblemVaultSDK;
---
+++ types/utils.d.ts
@@ -0,0 +1,18 @@
+export declare const NFT_DATA: any;
+export declare const pad: (num: string | any[], size: number) => string | null;
+export declare const evaluateFacts: (allowed: boolean, facts: {
+    eval: any;
+    msg: string;
+}[], msgCallback: any) => boolean;
+export declare const metadataObj2Arr: (data: any) => any[];
+export declare const metadataAllProjects: (projects: any[]) => any[];
+export declare const fetchData: (url: string, apiKey: string, method?: string, body?: any) => Promise<any>;
+export declare function generateAttributeTemplate(record: any): any;
+export declare function generateImageTemplate(record: any): any;
+export declare function generateTemplate(record: any): any;
+export declare function templateGuard(input: {
+    [x: string]: any;
+    hasOwnProperty: (arg0: string) => any;
+}): void;
+export declare function genericGuard(input: any, type: string, key: string): void;
+export declare const COIN_TO_NETWORK: any;
