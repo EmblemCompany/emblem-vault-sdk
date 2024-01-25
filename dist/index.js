1ee57864d368f3eaf7b9faf4f96d67ff23868b9b
shannon code <genecyber@gmail.com>
2024-01-25 08:32:35 -0500

    update collection metadata

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const bignumber_1 = require("@ethersproject/bignumber");
 const utils_1 = require("./utils");
-const SDK_VERSION = '1.7.3';
+const SDK_VERSION = '1.7.4';
 class EmblemVaultSDK {
     constructor(apiKey, baseUrl) {
         this.apiKey = apiKey;
