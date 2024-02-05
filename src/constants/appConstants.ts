8d4e98309d07a55f0d1c99085ccfcd991033cf8d
decentraliser <me@decentraliser.dev>
2024-02-05 15:23:28 +0700

    add get Torus key

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,2 @@
+export const WEB3_AUTH_DASHBOARD_CLIENT_ID =
+  'BDd7Lg7pUjr2_n5zTSiwPfauxJkNCXxWbh3mwrKUz1HejZrb6KtfPgQcCRfLtr-GfvbC0Oy8YktupkKCw2xi2B4'
---
+++ src/services/torus.ts
@@ -0,0 +1,36 @@
+import FetchNodeDetails from '@toruslabs/fetch-node-details'
+import TorusUtils from '@toruslabs/torus.js'
+
+import { WEB3_AUTH_DASHBOARD_CLIENT_ID } from '../constants/appConstants'
+
+export const getTorusKey = async (
+  tokenId: string,
+  signedJwt: any
+): Promise<string> => {
+  const fetchNodeDetails = new FetchNodeDetails({ network: 'mainnet' })
+  const torusUtils = new TorusUtils({
+    clientId: WEB3_AUTH_DASHBOARD_CLIENT_ID,
+    enableOneKey: true,
+    network: 'mainnet',
+  })
+  const {
+    torusNodeEndpoints, //
+    torusIndexes,
+  } = await fetchNodeDetails.getNodeDetails({
+    verifier: 'tor-us-signer-vercel',
+    verifierId: tokenId,
+  })
+
+  // @TEST
+  const {
+    finalKeyData: { privKey },
+  } = await torusUtils.retrieveShares(
+    torusNodeEndpoints,
+    torusIndexes,
+    'tor-us-signer-vercel',
+    { verifier_id: tokenId },
+    signedJwt
+  )
+  if (!privKey) throw new Error('No private key found')
+  return privKey
+}
